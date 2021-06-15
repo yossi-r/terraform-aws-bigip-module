41b701568a03c62d9011265ce5cf57f449de562b
yossi <yossi.ros@gmail.com>
2021-06-15 01:17:07 +0000

    instance_id

--- examples/bigip_aws_1nic_deploy/main.tf
+++ examples/bigip_aws_1nic_deploy/main.tf
@@ -184,7 +184,6 @@ resource "aws_key_pair" "generated_key" {
 #
 module bigip {
   source = "../../"
-  count  = var.instance_count
   prefix = format("%s-1nic", var.prefix)
   //ec2_key_name = aws_key_pair.generated_key.key_name
   f5_password = random_string.password.result
@@ -193,18 +192,6 @@ module bigip {
   //aws_iam_instance_profile    = aws_iam_instance_profile.instance_profile.name
   mgmt_subnet_ids        = [{ "subnet_id" = aws_subnet.mgmt.id, "public_ip" = true, "private_ip_primary" = "" }]
   mgmt_securitygroup_ids = [module.mgmt-network-security-group.security_group_id]
-}
-
-resource "null_resource" "clusterDO" {
-  count = var.instance_count
-  provisioner "local-exec" {
-    command = "cat > DO_1nic.json <<EOL\n ${module.bigip[count.index].onboard_do}\nEOL"
-  }
-  provisioner "local-exec" {
-    when    = destroy
-    command = "rm -rf DO_1nic.json"
-  }
-  depends_on = [module.bigip]
 }
 
 
